40adc52d9cf7619a52a2a249dc43eae56ee542ca
Praise Aribisala <aribisalapraise12@gmail.com>
2025-01-06 07:45:30 +0100

    Commit

---
+++ front-end/src/components/addVoterModal.jsx
@@ -0,0 +1,151 @@
+// import React from 'react'
+import { useState } from "react";
+import Modal from "react-modal";
+import { HiQuestionMarkCircle } from "react-icons/hi";
+import { CgDanger } from "react-icons/cg";
+
+Modal.setAppElement("#root"); //set app for accessibility
+
+
+export default function AddVoterModal({ isOpen, onRequestClose, onSave }) {
+    const [error, setError] = useState({});
+
+    const [formData, setFormData] = useState({
+        voter_name: '',
+        voter_id: '',
+        voter_key: '',
+        voter_email: '',
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({...formData, [name]: value});
+    }
+
+
+    const handleAddVoter = (e) => {
+        e.preventDefault();
+        setError({});
+        // // handleBallotSave
+        const newErrors = validate(formData);
+        setError(newErrors);
+        
+        if (Object.keys(newErrors).length === 0) {
+            onSave(formData);
+            onRequestClose();
+            console.log(formData);
+        }
+        // // Save
+        
+    }
+
+    const validate = (form) => {
+        const errors = {}
+        if (!form.voter_name) {
+            errors.voter_name = 'Title is required';
+        }
+
+        if (!form.voter_id) {
+            errors.voter_id = "Voter's Id is required";
+        }
+
+        if (!form.voter_key) {
+            errors.voter_key = "Voter's Key is required";
+        }
+
+        if (!form.voter_email) {
+            errors.voter_email = "Voter's email is required";
+        } else if (!/^\S+@\S+\.\S+$/i.test(form.voter_email)) {
+            errors.voter_email = 'Invalid email address;'
+        }
+
+
+        return errors;
+    }
+
+    // const handleRandom = (state) => {
+    //     setRandomEnabled(state);
+    //     console.log(randomEnabled);
+    // }
+
+    if (!isOpen) return null;
+  return (
+    <>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            contentLabel="Add Voter"
+            className='modal mt-[.5rem] mb-[1rem] overflow-y-auto scrollbar-hide no-scrollbar'
+            overlayClassName={`overlay bg-[#000] fixed top-0 left-0 right-0 bottom-0 flex flex-col items-center ${isOpen ? 'backdrop-blur-sm opacity-[.95]' : ''}`}
+        >
+            <div className="bg-[#fff] w-[90%] m-auto z-99">
+                <div className="flex justify-between px-[2rem] py-[1rem] bg-[#0bacfa]">
+                    <div className="text-[1.5rem] text-[#fff] font-semibold">Add Voter</div>
+                    <div className="text-[1rem] text-[#fff] font-bold" onClick={onRequestClose}>X</div>
+                </div>
+                <div className="px-[1rem] py-[1rem]">
+                    <form action="">
+                        <div className="form-group mt-[1rem] flex flex-col gap-1">
+                            <div className={`flex items-center gap-1 ${error.voter_name ? 'text-red-500' : 'text-[#000]'}`}>
+                                <label htmlFor="voter_name" className={`text-[1rem] font-bold`}>Name</label>
+                                {!error.voter_name && (<HiQuestionMarkCircle />)}
+                                {error.voter_name && (<CgDanger />)}
+                            </div>
+                            {error.voter_name && (
+                                <p className="text-red-600 text-[.8rem]">{error.voter_name}</p>
+                            )}
+                            <input type="text" onChange={handleChange} value={formData.voter_name} placeholder="Voter's Name" name="voter_name" className="bg-[#f6f8fa] border px-[.5rem] text-[1.1rem] py-[.4rem] border-[#ced4da] rounded-sm" />
+                        </div>
+                        <div className="flex whitespace-normal justify-between">
+                            <div className="form-group flex flex-col gap-1 mt-[1rem] w-[40%]">
+                                <div className={`flex items-center gap-1 ${error.voter_id ? 'text-red-500' : ''}`}>
+                                    <label htmlFor="voter_id" className={`text-[1rem] font-bold`}>Voter ID</label>
+                                    {!error.voter_id && (<HiQuestionMarkCircle />)}
+                                    {error.voter_id && (<CgDanger />)}
+                                </div>
+                                {/* <label htmlFor="voter-id" className={`text-[1rem] font-bold ${error.voter_id ? 'text-red-500' : ''}`}>Voter ID</label> */}
+                                {error.voter_id && (
+                                    <p className="text-red-600 text-[.8rem]">{error.voter_id}</p>
+                                )}
+                                <input type="text" name="voter_id" value={formData.voter_id} onChange={handleChange} placeholder="Voter's ID" className="bg-[#f6f8fa] border px-[.5rem] text-[1.1rem] py-[.4rem] border-[#ced4da] rounded-sm"></input>
+                            </div>
+                            <div className="form-group flex flex-col gap-1 mt-[1rem] w-[40%]">
+                                <div className={`flex items-center gap-1 ${error.voter_key ? 'text-red-500' : 'text-[#000]'}`}>
+                                    <label htmlFor="voter_key" className={`text-[1rem] font-bold`}>Voter Key</label>
+                                    {!error.voter_key && (<HiQuestionMarkCircle />)}
+                                    {error.voter_key && (<CgDanger />)}
+                                </div>
+                                {/* <label htmlFor="voter-key" className={`text-[1rem] font-bold ${error.voter_key ? 'text-red-500' : ''}`}>Voter Key</label> */}
+                                {error.voter_key && (
+                                    <p className="text-red-600 text-[.8rem]">{error.voter_key}</p>
+                                )}
+                                <input type="text" name="voter_key" value={formData.voter_key} onChange={handleChange} placeholder="Voter's Key" className="bg-[#f6f8fa] border px-[.5rem] text-[1.1rem] py-[.4rem] border-[#ced4da] rounded-sm"></input>
+                            </div>
+                        </div>
+
+                        <div className="form-group border-[#ced4da] flex flex-col gap-1 mt-[1rem]">
+                            <div className={`flex items-center gap-1 ${error.voter_email ? 'text-red-500' : 'text-[#000]'}`}>
+                                <label htmlFor="voter_email" className={`text-[1rem] font-bold`}>Voter Email</label>
+                                {!error.voter_email && (<HiQuestionMarkCircle />)}
+                                {error.voter_email && (<CgDanger />)}
+                            </div>
+                            {/* <label htmlFor="voter_email" className={`text-[.8rem] font-bold ${error.voter_email ? 'text-red-500' : ''}`}>Email Address</label> */}
+                            {error.voter_email && (
+                                <p className="text-red-600 text-[.8rem]">{error.voter_email}</p>
+                            )}
+                            <input type="text" name="voter_email" value={formData.voter_email} onChange={handleChange} placeholder="Voter's Email Address" className="bg-[#f6f8fa] border px-[.5rem] text-[1.1rem] py-[.4rem] border-[#ced4da] rounded-sm" />
+                        </div>
+
+                        <div className="form-group mt-[2rem] flex gap-2">
+                            <button type="submit" onClick={handleAddVoter} className="bg-[#2ecd10] text-white px-[1rem] py-[.5rem] rounded">Add Voter</button>
+                            <button type="submit" onClick={onRequestClose} className="border py-[.5rem] px-[1rem] rounded">
+                                Close
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </Modal>
+    </>
+  )
+}
--- front-end/src/pages/voters.jsx
+++ front-end/src/pages/voters.jsx
@@ -2,11 +2,16 @@ import { useState, useContext } from "react";
 import Sidebar from "../components/sidebar";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { TokenContext } from "../context/AuthContext";
+import { FaUsers } from "react-icons/fa";
+import { FaCloudArrowUp } from "react-icons/fa6";
+import { IoMdAdd } from "react-icons/io";
+import AddVoterModal from "../components/addVoterModal";
 
 export default function Settings() {
     const [isOpen, setIsOpen] = useState(false);
     const [isActive, setIsActive] = useState(false);
     const { electionDetails } = useContext(TokenContext);
+    const [openVoterModal, setOpenVoterModal] = useState(false);
     
 
     const handleButtonChange = () => {
@@ -14,24 +19,63 @@ export default function Settings() {
         setIsActive(!isActive);
     }
 
+    const handleAddVoter = () => {
+        setOpenVoterModal(true);
+    }
+
+    const handleVoterSave = (form) => {
+        if (form) {
+            console.log(form);
+        }
+    }
+
     return (
-    <div className="relative">
-        <div className={`fixed left-0 top-0 w-[12rem] h-full shadow-md transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 lg:block`}><Sidebar /></div>
-        <div className={`whitespace-nowrap duration-300 ease-in-out ${isOpen ? "ml-[12rem]": "ml-0"} md:ml-[12rem]`}>
-            <div className={`flex whitespace-nowrap w-[100%] gap-[0.2rem] items-center border border-l-0 border-r-0 border-t-0 `}>
-                <div className={`flex lg:hidden justify-center items-center w-[5%] px-[1.5rem] py-[1rem] border border-l-0 border-t-0 border-b-0 ${isActive ? 'bg-[#f2f2f2]' : 'bg-[#f6f8fa]'}`}>
-                    <button 
-                        onClick={handleButtonChange}
-                        className={``}
-                    >
-                        <GiHamburgerMenu />
-                    </button>
+        <>
+            <div className="relative">
+                <div className={`fixed left-0 top-0 w-[12rem] h-full shadow-md transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 lg:block`}><Sidebar /></div>
+                <div className={`whitespace-nowrap duration-300 ease-in-out ${isOpen ? "ml-[12rem]": "ml-0"} md:ml-[12rem]`}>
+                    <div className={`flex whitespace-nowrap w-[100%] gap-[0.2rem] items-center border border-l-0 border-r-0 border-t-0 `}>
+                        <div className={`flex lg:hidden justify-center items-center w-[5%] px-[1.5rem] py-[1rem] border border-l-0 border-t-0 border-b-0 ${isActive ? 'bg-[#f2f2f2]' : 'bg-[#f6f8fa]'}`}>
+                            <button 
+                                onClick={handleButtonChange}
+                                className={``}
+                            >
+                                <GiHamburgerMenu />
+                            </button>
+                        </div>
+                        <div className="font-bold px-[1rem] py-[1rem]">{electionDetails.title || "Election Name"}</div>
+                    </div>
+                    <div className="whitespace-nowrap">
+                        <div className="page-content bg-white flex flex-col items-center justify-center h-screen">
+                            <div className="flex items-center gap-3 text-[#212529] font-semibold">
+                                <FaUsers className="text-[4rem]" />
+                                <p className="text-[2.25rem]">Add Voters</p>
+                            </div>
+                            <p className="text-[1.5rem] text-[#212529]">Add voters to this election.</p>
+                            <div  className="flex items-center gap-1 text-[1.3rem] mt-[1rem]">
+                                <button className="flex items-center gap-1 border border-[#0bacfa] text-[#0bacfa] px-[1rem] py-[0.5rem] rounded-sm">
+                                    <FaCloudArrowUp />
+                                    Import
+                                </button>
+                                <button 
+                                    className="flex items-center gap-1  font-semibold border border-[#2ecd10] bg-[#2ecd10] py-[0.5rem] px-[1rem] rounded-sm text-[#fff]"
+                                    onClick={handleAddVoter}
+                                >
+                                    <IoMdAdd />
+                                    Add Voter
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="font-bold px-[1rem] py-[1rem]">{electionDetails.title || "Election Name"}</div>
+                
             </div>
-            <div className="whitespace-nowrap">Hi</div>
-        </div>
-        
-    </div>
+
+            <AddVoterModal 
+                isOpen={openVoterModal}
+                onRequestClose={() => setOpenVoterModal(false)}
+                onSave={handleVoterSave}
+            />
+        </>
     )
 }
